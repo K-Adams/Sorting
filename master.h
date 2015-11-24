7c03e1d42feddbe3d7c94739340eefed86196c62
K-Adams <kbadams@uga.edu>
2015-11-23 22:14:10 -0500

    Updated Master.h to ?working quicksort
    updated quick sort() and partition()

--- master.h
+++ master.h
@@ -1,45 +1,60 @@
 #include <iostream>
 #include< stdio.h>
 #include <math.h>
+#include <utility>
+#include <algorithm>
 
 using std namespace;
 
 // the following is from the pseudocode in chapter 7 powerpoint
 
-int Partition(char *A, int p, int r)
+int Partition(int *A, int left, int right)
 {
-  char x = A[r];
-  int i = p-1;
+int middle = left + (right - left)/2;
+int pivot = A[middle];
+
+//if doesnt work use std::swap()....
+swap(A[middle], A[left]);
+
+  int i = left-1;
   int j;
-  for(j = p; j<r-1; j++)
+  for(j = left; j<r-1; j++)
     {
-      if(A[j] <= x)
+      if(A[j] <= pivot)
 	{
 	  i = i+1;
 	  A[i] = A[j];
 	}
-      A[i+1]= A[r];
+      A[i+1]= A[right];
     }
   return i + 1;
 }
 
-void QuickSort(char *A, int p, int r)
+void QuickSort(int *A, int left, int right, const int size)
 {
-  if(p < r)
+	int p = partition(A, left, right);
+	cout << "QSC:" << left << "," << right << " part=" << part << "\n";
+	
+  if(left < p - right)
     {
-      q = Partition(A,p,r);
-      QuickSort(A, p, q-1);
-      QuickSort(A, q+1, r);
+      QuickSort(A, left, p-1);
+      QuickSort(A, p+1, right);
+    }
+    
+    if(p + 1 < right)
+    {
+    	Quicksort(A, p + 1, right, size);
     }
 }
 
+/*
 void InsertionSort(char* A)
 {
   int j;
 
   for(j=2; j<A.size; j++)//look up using .size for an array in c++
     {
-      /* insert A[j] into the sorted sequence A[1 ... j-1] */
+      // insert A[j] into the sorted sequence A[1 ... j-1] 
       i = j-1;
       while(i > 0 && A[i] > key)
 	{
@@ -60,3 +75,4 @@ void MergeSort(char* A, int p, int r)
       MergeSort(A,p, q, r);
     }
 }
+*/
