a4c41c2749aff3fbbc107f44b0be396f01a0a982
Kollin <kbadams@uga.edu>
2015-11-25 19:24:18 -0500

    Parsing bugs fixed. NEED to fix output to SPECS

--- sort.cpp
+++ sort.cpp
@@ -8,7 +8,9 @@
 #include <stdlib.h>
 using namespace std;
 
-//UPDATE
+//UPDATE: cmd line input Parsing bugs FIXED
+//Quicksort function WORKING
+//TODO: NEED TO FIX UP OUTPUT TO SPECS
 int main(int argc, char * argv[])
 {
   int size = 0;
@@ -35,16 +37,22 @@ int main(int argc, char * argv[])
   //}
   while((strncmp (argv[1],"-q",2) == 0))
   {
-    if(index <= (argc - 2))
-    {
-      input[index] = atoi(argv[2 + index]);
-      cout<<"Input: "<<input[index]<<endl;
-      index++;
-      size++;
-    }
-    QuickSort(B, input[0], input[size-1], size);//issues with the first parameter potentially seg fault
-    cout<<endl;
-    cout<<"Input size: "<< size<< endl;
+    if(index < (argc - 2))
+      {
+	input[index] = atoi(argv[2 + index]);//seg faults here. loop continues to enter conditional after parsing all inputs. cannot halt
+	cout<<"Input: "<<input[index]<<endl;
+	index++;
+	size++;
+      }
+    if(index == (argc - 2))
+      {
+	input[index+1] = '\0';
+	break;
+      }
+    QuickSort(B, input[0], input[size-1], size);
   }  
+  //QuickSort(B, input[0], input[size-1], size);//Quicksort is operating as intended                                                                                        
+  cout<<endl;
+  cout<<"Input size: "<< size<< endl;
   return EXIT_SUCCESS;
 }
