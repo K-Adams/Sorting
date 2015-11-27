c528df981fb92e27a06ff50087e4c6e50f97c264
awilburn <awilburn@uga.edu>
2015-11-27 18:56:23 -0500

    austin update1
    hopefully insertion sort working properly

--- master.h
+++ master.h
@@ -64,17 +64,21 @@ void InsertionSort(int *A, int size)
   int j;
   int i;
   int key = 0;
-  for(j=2; j<size; j++)
+  for(j=1; j<size; j++)
     {
+	c++;
+	key = A[j];
       // insert A[j] into the sorted sequence A[1 ... j-1] 
       i = j-1;
-      while(i > 0 && A[i] > key)
+      while(i >= 0 && A[i] > key)
 	{
 	  A[i+1] = A[i];
 	  i = i-1;
+	c++;
 	}
       A[i+1] = key;
     }
+	c++;
 }
 
 //Merge sort psuedocode pulled from ch1-3 ppt
@@ -96,19 +100,19 @@ void MergeSort(int *A, int p, int r)
 
 void Merge(int *A, int p, int q, int r)
 {
- n1 = q – p + 1
- n2 = r – q
-Let L[1 … n1 + 1] and R[1 … n2 + 1] be new arrays
+ n1 = q â€“ p + 1
+ n2 = r â€“ q
+Let L[1 â€¦ n1 + 1] and R[1 â€¦ n2 + 1] be new arrays
 for i =1 to n1
     L[i] = A[p + i - 1] 
 for j = 1 to n2 
     R[j] = A[q + j]
- L[n1 + 1] = 
-R[n2 + 1] = 
+ L[n1 + 1] = ï‚¥
+R[n2 + 1] = ï‚¥
 i = 1
 j = 1
  for k = p to r
-    if L[i]  R[j]
+    if L[i] ï‚£ R[j]
          A[k] = L[i]
          i = i + 1
       else A[k] = R[j]
